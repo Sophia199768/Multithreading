6112f4efe0d9c956ff1f08912eb03e4e84710baf
sophia <sofiya.brovkina@inbox.ru>
2025-03-23 11:00:30 +0300

    add second lab

--- server/main.cpp
+++ server/main.cpp
@@ -3,9 +3,10 @@
 int main() {
     boost::asio::io_context io_context;
     Server server(io_context, 12345);
+    int amount_of_threads = 5;
 
     boost::thread_group threads;
-    for (int i = 0; i < 4; ++i) {
+    for (int i = 0; i < amount_of_threads; ++i) {
         threads.create_thread([&io_context]() { io_context.run(); });
     }
     threads.join_all();
